e82f3943a19880258c660fa86d62ef87e1a4d1e5
UHANKNAG <hhs095615@gmail.com>
2024-07-02 16:00:34 +0900

    0702
    stage 1 지형 추가

---
+++ Assets/Scripts/CameraFollower.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollower : MonoBehaviour
+{
+    public float followSpeed = 2f;
+    public Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 camPos = new Vector3(player.position.x + 3f, 0f, -10f);
+
+        if (player.position.x + 3f < 0f)
+        {
+            transform.position = new Vector3(0, 0, -10f);
+        }
+        transform.position = Vector3.Slerp(transform.position, camPos, followSpeed * Time.deltaTime);
+    }
+}
