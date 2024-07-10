8d62c9a9053408200e175c76b9bda653c0db11d6
TrashTrain <96978670+TrashTrain@users.noreply.github.com>
2024-07-10 16:42:42 +0900

    소리 설정 음소거 기능 구현

--- Assets/Scripts/UI/SoundControl.cs
+++ Assets/Scripts/UI/SoundControl.cs
@@ -1,44 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class SoundControl : MonoBehaviour
 {
     [Header("soundCheck")]
-    public GameObject soundCheckT;
-    public GameObject soundCheckF;
+    public GameObject[] soundCheckT;
+    public GameObject[] soundCheckF;
 
     [Header("soundBarControl")]
-    public GameObject soundBarValue;
-    public GameObject effectBarValue;
+    [SerializeField] private Slider m_MusicMasterSlider;
+    [SerializeField] private Slider m_MusicBGMSlider;
+    [SerializeField] private Slider m_MusicSFXSlider;
 
-    [Header("SoundManager")]
-    public GameObject bgm;
+    [Header("MixerGroup")]
+    public AudioMixerGroup bgm;
+
+    [Header("AudioMixer")]
+    public AudioMixer audioMixer;
+
+    private string[] mixerNames = { "Master", "BGM", "SFX" };
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        soundBarValue.GetComponent<Slider>().value = 0.5f;
-        effectBarValue.GetComponent<Slider>().value = 0.5f;
+        //m_MusicMasterSlider.GetComponent<Slider>().value = 0.5f;
+        //m_MusicBGMSlider.GetComponent<Slider>().value = 0.5f;        
+        m_MusicMasterSlider.onValueChanged.AddListener(SetMasterVolume);
+        m_MusicBGMSlider.onValueChanged.AddListener(SetBGMVolume);
+        m_MusicSFXSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
     // Update is called once per frame
     void Update()
     {
-        bgm.GetComponent<AudioSource>().volume = soundBarValue.GetComponent<Slider>().value;
+        
+    }
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+
+    }
+    public void SetBGMVolume(float volume)
+    {
+        audioMixer.SetFloat("BGM", Mathf.Log10(volume) * 20);
+    }
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
     }
 
-    public void SoundButtonClick()
+    public void OnVolume(float volume, string exposedName, bool _isVolume)
     {
-        if (soundCheckT.activeSelf)
+        Debug.Log(_isVolume);
+        if (_isVolume)
         {
-            soundCheckT.SetActive(false);
-            soundCheckF.SetActive(true);
+            audioMixer.SetFloat(exposedName, Mathf.Lerp(-80f, 0f, Mathf.Clamp01(volume)));
         }
         else
         {
-            soundCheckT.SetActive(true);
-            soundCheckF.SetActive(false);
+            audioMixer.SetFloat(exposedName, Mathf.Lerp(0f, 0f, Mathf.Clamp01(volume)));
+
+        }
+    }
+    public void SoundButtonClick(int select)
+    {
+        if (soundCheckT[select].activeSelf)
+        {
+            soundCheckT[select].SetActive(false);
+            soundCheckF[select].SetActive(true);
+            OnVolume(0, mixerNames[select], soundCheckF[select].activeSelf);
+        }
+        else
+        {
+            soundCheckT[select].SetActive(true);
+            soundCheckF[select].SetActive(false);
+            OnVolume(0, mixerNames[select], soundCheckF[select].activeSelf);
         }
     }
 }
