4a1046d84e3e2966ca7c2b2d464931b7466d59a6
AcuaRi <81779747+AcuaRi@users.noreply.github.com>
2024-07-11 15:07:17 +0900

    Add LoadScene with Loading Bar

---
+++ Assets/Scripts/LoadingScene/SceneLoader.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public static async void LoadScene(string sceneName)
+    {
+        // 로딩 씬을 먼저 로드
+        await LoadLoadingScene();
+
+        // 로딩 씬의 스크립트를 통해 실제 씬을 비동기적으로 로드
+        LoadingScreen loadingScreen = LoadingScreen.Instance;
+        if (loadingScreen != null)
+        {
+            await loadingScreen.LoadSceneAsync(sceneName);
+        }
+    }
+
+    private static async UniTask LoadLoadingScene()
+    {
+        // 비동기 씬 로드 시작
+        var operation = SceneManager.LoadSceneAsync("LoadingScene");
+        await UniTask.WaitUntil(() => operation.isDone);
+    }
+}
