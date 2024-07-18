d92c97b335c62b9a640809cbbe6f06bea049e2c0
TrashTrain <96978670+TrashTrain@users.noreply.github.com>
2024-07-18 16:06:09 +0900

    코드 변경으로 인한 수정사항

--- Assets/Scripts/UI/NPC/SlotController.cs
+++ Assets/Scripts/UI/NPC/SlotController.cs
@@ -1,10 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SlotController : MonoBehaviour
 {
-
+    public SlotData[] slotDataGroup;
+    public Dictionary<string, SlotData> slotDB;
+    
+    [Header("Slot1")]
+    public Image slot1Image;
+    public TextMeshProUGUI slot1Name;
+    public TextMeshProUGUI slot1Description;
+    [Header("Slot2")]
+    public Image slot2Image;
+    public TextMeshProUGUI slot2Name;
+    public TextMeshProUGUI slot2Description;
+    [Header("Slot3")]
+    public Image slot3Image;
+    public TextMeshProUGUI slot3Name;
+    public TextMeshProUGUI slot3Description;
     void Start()
     {
         
